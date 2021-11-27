7205377b233a73d0d4c3c2b3c51bcb39d7f31517
MahendiPatel <mahendi.mpatel@gmail.com>
2021-11-26 23:03:23 -0500

    Added comment.

--- src/main/java/com/auto/pages/landingPage.java
+++ src/main/java/com/auto/pages/landingPage.java
@@ -5,14 +5,14 @@ import org.openqa.selenium.WebDriver;
 
 public class landingPage extends WebActionUtils {
     WebDriver driver;
-    static String pageName = landingPage.class.getSimpleName(); //"LandingPage"
+    static String pageName = landingPage.class.getSimpleName();
 
     public landingPage(WebDriver driver) {
         super(driver, pageName);
         this.driver = driver;
     }
 
-
+    //function is for navigate to "amazon.com"
     public void navigateToWebsite(String url) throws Exception {
         launchUrl(url);
         Thread.sleep(2000);
